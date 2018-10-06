0260cd5ef474ac6be1ec7b1e3f7bcbc2e719d476
Deivid Veloso <veloso.deivid@gmail.com>
2018-10-06 13:27:29 -0300

    tests

--- src/App.js
+++ src/App.js
@@ -31,43 +31,86 @@ class App extends Component {
   //   console.log(obj);
   // };
 
-  handleGroupBy = (items, key) => {
-    let arr = [];
-    // let keys = [];
-    let count = 0;
-    items.map(item => {
-      let obj = {};
-      if (item[key]) {
-        obj[item[key]] = item;
+  // handleGroupBy = (items, key) => {
+  //   let arr = [];
+  //   // let keys = [];
+  //   let count = 0;
+  //   items.map(item => {
+  //     let obj = {};
+  //     if (item[key]) {
+  //       obj[item[key]] = item;
 
-        if (arr.length > 0) {
-          console.log("ARR", Object.keys(arr[count]).toString());
-          console.log("item[key]", item[key]);
+  //       if (arr.length > 0) {
+  //         console.log("ARR", Object.keys(arr[count]).toString());
+  //         console.log("item[key]", item[key]);
 
-          if (Object.keys(arr[count]).toString() === item[key]) {
-            count++;
-          }
+  //         if (Object.keys(arr[count]).toString() === item[key]) {
+  //           count++;
+  //         }
+  //       }
+
+  //       arr.push(obj);
+  //       // console.log(Object.keys(arr));
+  //       // if (arr[index].includes(item[key])) {
+  //       //   console.log("Já tem");
+  //       // }
+  //     } else {
+  //       arr.push(item);
+  //     }
+  //   });
+
+  // let novo = [];
+  // keys.map((key, index) => {
+  //   arr.map(item => {
+  //     console.log("ITEM", item);
+  //     console.log("KEY", item[key]);
+  //   });
+  // });
+
+  // console.log("NOVO", arr);
+  // };
+
+  // handleGroupBy = (items, key) => {
+  //   Array.prototype.groupBy = function(prop) {
+  //     return this.reduce(function(groups, item) {
+  //       const val = item[prop];
+  //       groups[val] = groups[val] || [];
+  //       groups[val].push(item);
+  //       return groups;
+  //     }, {});
+  //   };
+  //   console.log(items.groupBy(key));
+  // };
+
+  handleMatrizReduce = () => {
+    const numbers = [
+      [1, 4, 0, 6, -7, 0],
+      [12, 2, -4, -90, 0, 0],
+      [87, 0, 0, -60, 0, 1],
+      [3, 1, 2, 7, 8, -9]
+    ];
+
+    let novo = numbers.map((nums, index) => {
+      return nums.reduce((acc, next) => {
+        if (next > 0) {
+          return (acc += next);
         }
-
-        arr.push(obj);
-        // console.log(Object.keys(arr));
-        // if (arr[index].includes(item[key])) {
-        //   console.log("Já tem");
-        // }
-      } else {
-        arr.push(item);
-      }
+        return acc;
+      }, 0);
     });
 
-    // let novo = [];
-    // keys.map((key, index) => {
-    //   arr.map(item => {
-    //     console.log("ITEM", item);
-    //     console.log("KEY", item[key]);
-    //   });
-    // });
+    console.log("SUMReduce", novo);
 
-    console.log("NOVO", arr);
+    var sum = [0, 0, 0, 0];
+    for (let i = 0; i < numbers.length; i++) {
+      for (let j = 0; j < numbers[i].length; j++) {
+        if (numbers[i][j] > 0) {
+          sum[i] = sum[i] + numbers[i][j];
+        }
+      }
+    }
+
+    console.log("SUM", sum);
   };
 
   handleClick = () => {
@@ -94,7 +137,7 @@ class App extends Component {
             alignItems: "center"
           }}
         >
-          {this.handleGroupBy(carros, "marca")}
+          {this.handleMatrizReduce(carros, "marca")}
           <Card>
             <div style={{ flex: 1 }}>
               <img
